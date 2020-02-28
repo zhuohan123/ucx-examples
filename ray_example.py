367647c2cabba339af37f71508d0820d3fabf310
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-27 20:33:32 -0800

    debug

--- ray_example.py
+++ ray_example.py
@@ -37,6 +37,7 @@ class Server:
 @ray.remote(num_gpus=1)
 class Client:
     async def run_concurrent(self):
+        time.sleep(2)
         ep = await ucp.create_endpoint(host, port)
         msg = np.zeros(n_bytes, dtype='u1') # create some data to send
         msg_size = np.array([msg.nbytes], dtype=np.uint64)
@@ -56,7 +57,6 @@ if __name__ == "__main__":
     ray.init()
     server = Server.remote()
     server_coroutine = server.run_concurrent.remote()
-    time.sleep(1)
     client = Client.remote()
     client_coroutine = client.run_concurrent.remote()
     ray.get([server_coroutine, client_coroutine])
