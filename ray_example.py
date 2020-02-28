80efe3db98357d547dd1de431e8cc34e821da1e4
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-27 20:44:43 -0800

    torch example

--- ray_example.py
+++ ray_example.py
@@ -5,29 +5,22 @@ import torch
 import numpy as np
 import ray
 
-n_bytes = 2**30
+n_elements = 2**10
 host = "127.0.0.1"
 port = 13337
 
 @ray.remote(num_gpus=1)
 class Server:
     async def run_concurrent(self):
-        self.my_data = torch.rand(10)
+        self.my_data = torch.rand(n_elements, dtype=torch.float, device='cuda')
         print("server data:", self.my_data)
         self.lf = ucp.create_listener(self.call_back, port)
         while not self.lf.closed():
             await asyncio.sleep(0.1)
 
     async def call_back(self, ep):
-        arr = np.empty(n_bytes, dtype='u1')
-        await ep.recv(arr)
-        assert np.count_nonzero(arr) == np.array(0, dtype=np.int64)
-        print("Received NumPy array")
-
-        # increment array and send back
-        arr += 1
-        print("Sending incremented NumPy array")
-        await ep.send(arr)
+        print("Sending data to client")
+        await ep.send(self.my_data)
         await ep.close()
 
     def get_node_ip(self):
@@ -39,19 +32,12 @@ class Client:
     async def run_concurrent(self):
         time.sleep(2)
         ep = await ucp.create_endpoint(host, port)
-        msg = np.zeros(n_bytes, dtype='u1') # create some data to send
-        msg_size = np.array([msg.nbytes], dtype=np.uint64)
+        msg = torch.empty(n_bytes, dtype=torch.float, device='cuda') # create some data to send
 
-        # send message
-        print("Send Original NumPy array")
-        await ep.send(msg, msg_size)  # send the real message
-
-        # recv response
-        print("Receive Incremented NumPy arrays")
-        resp = np.empty_like(msg)
-        await ep.recv(resp, msg_size)  # receive the echo
+        print("Receive data from server")
+        await ep.recv(msg)
         await ep.close()
-        np.testing.assert_array_equal(msg + 1, resp)
+        print("client data:", msg)
 
 if __name__ == "__main__":
     ray.init()
