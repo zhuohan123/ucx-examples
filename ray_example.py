7a8425ce0d5e12e3699f5d9ff040b7befc95ce4a
Zhuohan Li <zhuohan123@vip.qq.com>
2020-02-27 20:32:43 -0800

    add client

--- ray_example.py
+++ ray_example.py
@@ -6,6 +6,7 @@ import numpy as np
 import ray
 
 n_bytes = 2**30
+host = "127.0.0.1"
 port = 13337
 
 @ray.remote(num_gpus=1)
@@ -33,7 +34,29 @@ class Server:
         """Returns the IP address of the current node."""
         return ray.services.get_node_ip_address()
 
+@ray.remote(num_gpus=1)
+class Client:
+    async def run_concurrent(self):
+        ep = await ucp.create_endpoint(host, port)
+        msg = np.zeros(n_bytes, dtype='u1') # create some data to send
+        msg_size = np.array([msg.nbytes], dtype=np.uint64)
+
+        # send message
+        print("Send Original NumPy array")
+        await ep.send(msg, msg_size)  # send the real message
+
+        # recv response
+        print("Receive Incremented NumPy arrays")
+        resp = np.empty_like(msg)
+        await ep.recv(resp, msg_size)  # receive the echo
+        await ep.close()
+        np.testing.assert_array_equal(msg + 1, resp)
+
 if __name__ == "__main__":
     ray.init()
     server = Server.remote()
-    ray.get(server.run_concurrent.remote())
+    server_coroutine = server.run_concurrent.remote()
+    time.sleep(1)
+    client = Client.remote()
+    client_coroutine = client.run_concurrent.remote()
+    ray.get([server_coroutine, client_coroutine])
